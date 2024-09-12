8969a4c08685ebb21662b217d05b2a65faea51b6
Danish Maknojia <danishmak@Danishs-MacBook-Pro.local>
2024-09-12 13:51:09 -0400

    Save of main

--- mainfile.py
+++ mainfile.py
@@ -1,3 +1,17 @@
-def add(x, y, z):
+# Open up CSV File
 
-    return x + y + z
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# Read CSV
+df = pd.read_csv("Fantasy_Football_Projections_RB.csv")
+
+# Print DF
+print(df)
+
+# Group data by team and FPTS
+
+
+# Get description
+descriptioncsv = df.describe()
+print(descriptioncsv)
