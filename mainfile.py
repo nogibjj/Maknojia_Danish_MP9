2132fd5e64585ea987368bd054be468bcac78cc9
Danish Maknojia <danishmak@Danishs-MBP.lan>
2024-09-13 21:05:14 -0400

    First graph

--- mainfile.py
+++ mainfile.py
@@ -7,11 +7,33 @@ import matplotlib.pyplot as plt
 df = pd.read_csv("Fantasy_Football_Projections_RB.csv")
 
 # Print DF
-print(df)
+# print(df)
 
 # Group data by team and FPTS
+new_df = pd.DataFrame(df.groupby("Team")["FPTS"].sum()).reset_index(drop=False)
+
+# Make the new df sort by aesending order
+new_df = new_df.sort_values(by="FPTS", ascending=True)
+
+print(new_df)
 
 
-# Get description
-descriptioncsv = df.describe()
+# Get summary statistics
+descriptioncsv = new_df.describe()
 print(descriptioncsv)
+print(new_df)
+
+# Creating bar chart
+# Setting x to nfl team column and y to fantasy points column
+x = new_df["Team"]
+y = new_df["FPTS"]
+# Plotting the  bar chart
+plt.bar(x, y, width=0.8, color="orange")
+# Rotating all x-axis labels vertically that way they fit on the chart
+plt.xticks(range(len(x)), x, rotation="vertical")
+# Labeling chart
+plt.title("Projected Total RB Fantasy Football Points By NFL Team")
+plt.xlabel("NFL Team")
+plt.ylabel("Running Backs Total Fantasy Football Points 2024")
+# Show the chart
+plt.show()
